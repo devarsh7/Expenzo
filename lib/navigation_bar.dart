145d6a539a4f9379d9d6a162f57f900b9df03f47
Devarsh7 <devarsh.shahs2000@gmail.com>
2024-09-19 23:31:31 -0400

    Added navigation bar for the app

---
+++ lib/navigation_bar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:expenzo/home_screen.dart';
+import 'package:expenzo/history_screen.dart';
+import 'package:expenzo/settings_screen.dart';
+
+class MainContainer extends StatefulWidget {
+  @override
+  _MainContainerState createState() => _MainContainerState();
+}
+
+class _MainContainerState extends State<MainContainer> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomeScreen(),
+    HistoryScreen(),
+    SettingsScreen(),
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _children,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
