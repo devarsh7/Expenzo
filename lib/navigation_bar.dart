452f1b13d1c977ac960629ea223a3b58dde8a470
Devarsh7 <devarsh.shahs2000@gmail.com>
2024-09-28 23:06:18 -0400

    Added reports menu and modified the bar with animations

--- lib/navigation_bar.dart
+++ lib/navigation_bar.dart
@@ -1,4 +1,7 @@
+import 'package:expenzo/analytics.dart';
+import 'package:expenzo/reports_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:expenzo/home_screen.dart';
 import 'package:expenzo/history_screen.dart';
 import 'package:expenzo/settings_screen.dart';
@@ -12,7 +15,7 @@ class _MainContainerState extends State<MainContainer> {
   int _currentIndex = 0;
   final List<Widget> _children = [
     HomeScreen(),
-    HistoryScreen(),
+    ReportsScreen(),
     SettingsScreen(),
   ];
 
@@ -29,23 +32,20 @@ class _MainContainerState extends State<MainContainer> {
         index: _currentIndex,
         children: _children,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTabTapped,
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: 'History',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: _currentIndex,
+        height: 60.0,
+        items: <Widget>[
+          Icon(Icons.home, size: 30, color: Colors.white),
+          Icon(Icons.analytics_outlined, size: 30, color: Colors.white),
+          Icon(Icons.settings, size: 30, color: Colors.white),
         ],
+        color: Color(0xFF5C6BC0),
+        buttonBackgroundColor: Color(0xFF5C6BC0),
+        backgroundColor: Colors.white,
+        animationCurve: Curves.easeInOut,
+        animationDuration: Duration(milliseconds: 600),
+        onTap: onTabTapped,
       ),
     );
   }
